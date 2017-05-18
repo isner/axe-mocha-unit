917f47d8a969f3702df644bc190c3c28ddea9daf
Matt Isner <isnerms@gmail.com>
2017-05-18 09:50:16 -0400

    changes some file names

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ const gulp = require('gulp')
 const mocha = require('gulp-mocha-phantomjs')
 
 gulp.task('default', () => {
-  gulp.src('test/index.html')
+  gulp.src('test/fixture.html')
     .pipe(mocha({ reporter: 'spec' }))
 })
